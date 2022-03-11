be7d88e1db9de89db400e7b45b6794449b4e2565
Nicolas Puegher <nicopuegher@gmail.com>
2022-03-11 20:43:17 -0300

    add blocks movement

--- javascript/logic/blocksMovement.js
+++ javascript/logic/blocksMovement.js
@@ -1,7 +1,37 @@
 import checkRow from '../helpers/checkRow.js';
+import updateBlankPosition, { blankPosition } from './updateBlankPosition.js';
+
+const tablesData = document.getElementsByTagName('td');
+
+const movement = (id) => {
+  tablesData[blankPosition].innerHTML = tablesData[id].innerHTML;
+  tablesData[id].innerHTML = '';
+  updateBlankPosition(tablesData[id]);
+  // do count down here
+};
 
 const blocksMovement = (id, size) => {
-  checkRow(id);
+  const valid = checkRow(id);
+
+  let move = blankPosition - id;
+
+  if (move < 0) {
+    move = -move;
+  }
+
+  if (move === 1) {
+    if (!valid) {
+      return;
+    }
+
+    movement(id);
+  }
+
+  if (move === size) {
+    movement(id);
+  }
+
+  // check win here
 };
 
 export default blocksMovement;
